a7a23c91e27ab720a16d39baa3b26cd1782e449e
SophiaKohn <161403892+SophiaKohn@users.noreply.github.com>
2024-02-27 20:24:26 -0500

    Update and rename rule03example.txt to R03_NUM10_J.java
    add corrected code and rename

--- R03_NUM10_J.java
+++ R03_NUM10_J.java
@@ -5,3 +5,12 @@
 // Prints 0.1000000000000000055511151231257827021181583404541015625
 // when run in FP-strict mode
 System.out.println(new BigDecimal(0.1));
+
+
+//Corrected code:
+//Rule 03: User example
+//NUM10-J
+
+// Prints 0.1
+// when run in FP-strict mode
+System.out.println(new BigDecimal("0.1"));
