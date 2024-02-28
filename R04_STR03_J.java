669b7355b8f7d5e9e065669ef6e6dc8f516f5cc8
SophiaKohn <161403892+SophiaKohn@users.noreply.github.com>
2024-02-27 20:26:17 -0500

    Update and rename rule04.txt to R04_STR03_J.java
    corrected code and renamed

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -7,3 +7,11 @@ byte[] byteArray = x.toByteArray();
 String s = new String(byteArray);
 byteArray = s.getBytes();
 x = new BigInteger(byteArray);
+
+
+//Corrected code:
+BigInteger x = new BigInteger("530500452766");
+String s = x.toString(); //Valid character data
+byte[] byteArray = s.getBytes();
+String ns = new String(byteArray);
+x = new BigInteger(ns);
